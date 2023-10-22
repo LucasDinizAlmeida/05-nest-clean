4ba9d2e5d7ae1cb3888ef7aab3b3dcf8ee081219
lucas-linux <almeidalucas820@gmail.com>
2023-10-22 09:54:25 -0300

    Configurando Vitest com SWC

---
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,5 @@
+describe('Teste create account controller', () => {
+  it('should be possible to find the number 2', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
---
+++ vitest.config.e2e.ts
@@ -0,0 +1,19 @@
+import swc from 'unplugin-swc'
+import { configDefaults, defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    exclude: [...configDefaults.exclude, '**/data/pg/**'],
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+})
