a9ca817e39c83cf1c4e280c3912c0533c67ebe11
lucas-linux <almeidalucas820@gmail.com>
2023-10-24 19:28:01 -0300

    Criando schema do Prisma

--- src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -1,7 +1,9 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { Injectable } from '@nestjs/common'
 
+@Injectable()
 export class PrismaQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
